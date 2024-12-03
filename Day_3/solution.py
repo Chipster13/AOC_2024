87efb0a0e279e6915a45f848a9ff67a62f557be3
The Chipster <59063093+Chipster13@users.noreply.github.com>
2024-12-03 16:23:56 -0600

    Add files via upload

--- Day_3/solution.py
+++ Day_3/solution.py
@@ -1,22 +1,19 @@
 import regex as re
 
 data = open(0).read()
-results = 0
-groups = re.findall(r'mul\((-?\d{1,3}),(-?\d{1,3})\)', data)
-for group in groups:
-    results += int(group[0]) * int(group[1])
-print(f"Part 1: {results}")
-
-results = 0
+part1 = 0
+part2 = 0
 ignore = False
 groups = re.findall(r"mul\((-?\d{1,3}),(-?\d{1,3})\)|(don't\(\))|(do\(\))", data)
 for group in groups:
-    if group[2] == "don't()":
+    if "don't()" in group:
         ignore = True
-    elif group[3] == 'do()':
+    elif "do()" in group:
         ignore = False
     else:
+        part1 += int(group[0]) * int(group[1])
         if not ignore:
-            results += int(group[0]) * int(group[1])
+            part2 += int(group[0]) * int(group[1])
 
-print(f"Part 2: {results}")
+print(f"Part 1: {part1}")
+print(f"Part 2: {part2}")
