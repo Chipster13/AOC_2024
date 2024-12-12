bed7c3d10ff29352d59ee02e28c43d5825e87310
The Chipster <chipster13@jameskaufman.org>
2024-12-12 12:27:00 -0600

    Updated based on ideas from Hyper Neutrino

--- Day_11/solution.py
+++ Day_11/solution.py
@@ -1,20 +1,21 @@
-def calc_stones(stones, n):
-    for _ in range(n):
-        newstones = []
-        for stone in stones:
-            if stone == 0:
-                newstones.append(1)
-            elif len(str(stone)) % 2 == 0:
-                s = str(stone)
-                l = len(s) // 2
-                newstones.append(int(s[:l]))
-                newstones.append(int(s[l:]))
-            else:
-                newstones.append(stone * 2024)
+# Credit to HyperNeutrino for this cache fuction from functools
+# and learning about the @ decorator
 
-        stones = newstones
-    return len(stones)
+from functools import cache
 
-data = [int(x) for x in open(0).read().strip().split()]
-print(f"Part 1: {calc_stones(data, 25)}")
-#print(f"Part 2: {calc_stones(data, 75)}")
+@cache
+def calc_stones(stone, n):
+    if n == 0:
+        return 1
+    if stone == 0:
+        return calc_stones(1, n-1)
+    if  len(str(stone)) % 2 == 0:
+        s = str(stone)
+        l = len(s) // 2
+        return calc_stones(int(s[:l]), n-1) + calc_stones(int(s[l:]), n-1)
+    else:
+        return calc_stones(stone * 2024, n-1)
+
+data = [int(x) for x in open('in.txt').read().split()]
+print(f"Part 1: {sum(calc_stones(stone, 25) for stone in data)}")
+print(f"Part 2: {sum(calc_stones(stone, 75) for stone in data)}")
